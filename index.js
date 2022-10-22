4b501ebf58bfea2c3954fb6566be0e7e62ae1b23
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-22 12:39:03 +0100

    server file setup
    Setting up my server file, and connecting to MongoDB

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 //connecting to my MongoDB Server
-mongoose.connect('mongodb+srv://learning:Learning01@cluster0.4k9ccvn.mongodb.net/afiaohuru?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('DB Connect is a success'))
 .catch((err)=>{console.log(err)});
 
 app.listen(5000, () => {
     console.log("Hello I am listening!!");
-})
+});
