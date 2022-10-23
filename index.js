29f6bde43d89aa248f3af921809bb7058c0af553
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-23 14:07:03 +0100

    index.js update
    Adding the route for authentication of users to the index file

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const testRoute = require('./routes/user')
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 
@@ -11,7 +12,10 @@ mongoose.connect(process.env.MONGO_URL)
 .then(()=> console.log('DB Connect is a success'))
 .catch((err)=>{console.log(err)});
 
+//enable json
+app.use(express.json());
 //middleware for routes
+app.use('/api/auth', authRoute);
 app.use('/user', testRoute);
 
 app.listen(process.env.PORT || 5000, () => {
