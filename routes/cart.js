b4b064669221c29e715e63095267b32ac1f59a36
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-24 22:28:41 +0100

    Cart update
    Removing the Product Model import, not neede

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,4 @@
 const Cart = require('../models/Cart');
-const Product = require('../models/Cart');
 const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('./verifyToken');
 
 const router = require('express').Router();
