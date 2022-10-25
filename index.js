5c8616aa1821b96b2da5c4af8e8530c2a067cb5b
IgodoNdu <darkbluetechmail@gmail.com>
2022-10-25 17:09:15 +0100

    Stripe for payment
    Setting up stripe for payment processing

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const testRoute = require('./routes/user')
+const userRoute = require('./routes/user')
 const authRoute = require('./routes/auth');
+const productRoute = require('./routes/product');
+const cartRoute = require('./routes/cart');
+const orderRoute = require('./routes/order');
+//const stripeRoute = require('./routes/stripe');
 
 dotenv.config();
 
@@ -16,7 +20,11 @@ mongoose.connect(process.env.MONGO_URL)
 app.use(express.json());
 //middleware for routes
 app.use('/api/auth', authRoute);
-app.use('/user', testRoute);
+app.use('/api/user', userRoute);
+app.use('/api/product', productRoute);
+app.use('/api/cart', cartRoute);
+app.use('/api/order', orderRoute);
+//app.use('/api/payment', stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Hello I am listening!!");
